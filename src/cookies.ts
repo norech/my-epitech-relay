faed0ff088f5605836caebdb5d2e84b40f8e8dd7
redboard <ott.thomas68@gmail.com>
2022-11-12 13:41:48 +0100

    (+) adapting the code for more than 1 cookies.json and remove .env example

--- src/cookies.ts
+++ src/cookies.ts
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 import path_req from 'path';
 
 export async function saveCookies(page: puppeteer.Page, cookiesPath: string) {
+    console.log("3");
     const client = await page.target().createCDPSession();
     const cookies = (await client.send("Network.getAllCookies"))["cookies"];
     console.log("Writing cookies");
@@ -10,6 +11,7 @@ export async function saveCookies(page: puppeteer.Page, cookiesPath: string) {
 }
 
 export async function restoreCookies(page: puppeteer.Page, cookiesPath: string) {
+    console.log("4");
     if (!fs.existsSync(cookiesPath)) {
         console.log("No cookies to restore.");
         return;
@@ -25,9 +27,9 @@ export async function restoreCookies(page: puppeteer.Page, cookiesPath: string) 
 }
 
 export async function getCookiesFiles(path:string):Promise<Array<String>> {
-    const filenames = fs.readdirSync(path);
+    var filenames = fs.readdirSync(path);
     const jsonFiles = filenames.filter(function (file) {
         return path_req.extname(file) === ".json";
-    });
+      });
     return (jsonFiles);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 import puppeteer from 'puppeteer';
-import { restoreCookies, saveCookies } from './cookies';
+import { restoreCookies, saveCookies, getCookiesFiles } from './cookies';
 import express from "express";
 import axios from 'axios';
 const app = express();
@@ -17,16 +17,17 @@ async function openMicrosoftWindow(base: puppeteer.Page, url: string) {
     });
     const pages = await browser.pages();
     const page = pages[0];
-    await saveCookies(base, "./cookies.json");
-    await restoreCookies(page, "./cookies.json");
+    await saveCookies(base, "./cookies_bd/cookies.json");
+    await restoreCookies(page, "./cookies_bd/cookies.json");
     await page.goto(url);
     await page.waitForRequest((res) => res.url().startsWith("https://my.epitech.eu/"), { timeout: 0 });
-    await saveCookies(page, "./cookies.json");
-    await restoreCookies(base, "./cookies.json");
+    await saveCookies(page, "./cookies_bd/cookies.json");
+    await restoreCookies(base, "./cookies_bd/cookies.json");
     await browser.close();
 }
 
-async function refreshMyEpitechToken() {
+async function refreshMyEpitechToken(filePath: string) {
+    console.log("2");
     const loginBtnSelector = '[href^="https://login.microsoftonline.com/common/oauth2/authorize"]';
     const browser = await puppeteer.launch({
         executablePath: process.env.BROWSER_BINARY_PATH != ""
@@ -35,9 +36,11 @@ async function refreshMyEpitechToken() {
         args: process.env.BROWSER_ARGS?.split(" ") ?? [],
         headless: true
     });
+    
+
     const page = await browser.newPage();
     try {
-        await restoreCookies(page, "./cookies.json");
+        await restoreCookies(page, filePath);
         await page.goto('https://my.epitech.eu');
         const loginButton = await page.$(loginBtnSelector);
         if (loginButton != null) {
@@ -46,17 +49,21 @@ async function refreshMyEpitechToken() {
             await page.waitForNetworkIdle();
             const url = page.mainFrame().url();
             if (url.startsWith("https://login.microsoftonline.com/")) {
+                console.log("2.0");
                 console.log("Asking for oauth...");
-                await openMicrosoftWindow(page, url);
+                // await openMicrosoftWindow(page, url);
                 await page.reload();
                 await page.waitForNetworkIdle();
+                console.log("2.1");
             } else {
                 console.log("Auto-auth was successful");
             }
+            console.log("2.2");
         } else {
             console.log("Already logged in");
         }
     } catch (ex) {
+        console.log("2.3");
         await page.goto('https://my.epitech.eu');
         const loginButton = await page.$(loginBtnSelector);
         if (loginButton != null) {
@@ -64,6 +71,7 @@ async function refreshMyEpitechToken() {
             throw ex;
         }
     }
+    console.log("2.4");
     const token = await page.evaluate(() => localStorage.getItem("argos-api.oidc-token"));
     await browser.close();
     if (typeof token !== "string")
@@ -72,6 +80,7 @@ async function refreshMyEpitechToken() {
 }
 
 async function executeEpitestRequest(req: express.Request, token: string) {
+    console.log("1");
     const res = await axios({
         baseURL: "https://api.epitest.eu/",
         url: req.path,
@@ -86,26 +95,35 @@ async function executeEpitestRequest(req: express.Request, token: string) {
 }
 
 (async () => {
-    let myEpitechToken = await refreshMyEpitechToken();
+    console.log("0");
+    const pathDir = './cookies_bd/';
+    var fileList:Array<String> = await getCookiesFiles(pathDir);
+    console.log(fileList.length);
+    
+    for (var i = 0; i < fileList.length; ++i) {
+        const fileName = fileList[i].replace(".json", '');
+        const filePath = pathDir + fileList[i];
+        console.log(fileList[i], fileName, filePath);
 
-    app.get("/", (req, res) => {
-        res.send("the relay is working :D");
-    });
+        let myEpitechToken = await refreshMyEpitechToken(filePath);
+        app.get("/" + fileName + "/", (req, res) => {
+            res.send("the relay is working :D");
+        });
 
-    app.use("/epitest", async (req, res) => {
-        try {
-            let content = await executeEpitestRequest(req, myEpitechToken);
-            if (content.status == 401) {
-                myEpitechToken = await refreshMyEpitechToken();
-                content = await executeEpitestRequest(req, myEpitechToken);
+        app.use("/"+ fileName + "/epitest", async (req, res) => {
+            try {
+                let content = await executeEpitestRequest(req, myEpitechToken);
+                if (content.status == 401) {
+                    myEpitechToken = await refreshMyEpitechToken(filePath);
+                    content = await executeEpitestRequest(req, myEpitechToken);
+                }
+                res.status(content.status).send(content.data);
+            } catch (ex) {
+                console.error(ex);
+                res.status(500).send("Relay error.");
             }
-            res.status(content.status).send(content.data);
-        } catch (ex) {
-            console.error(ex);
-            res.status(500).send("Relay error.");
-        }
-    })
-
+        })
+    }
     const port = parseInt(process.env.PORT ?? "8080");
     const host = process.env.HOST ?? "127.0.0.1";
 
